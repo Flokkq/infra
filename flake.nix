f14506e1bbb34de1730db7a90901f485ef730e2a
Flokkq <webcla21@htl-kaindorf.at>
2025-01-02 03:00:06 +0100

    init(nix): basic top-level flake
    Signed-off-by: Flokkq <webcla21@htl-kaindorf.at>

---
+++ flake.nix
@@ -0,0 +1,66 @@
+{
+  description = "Top Level nixOS flake for my home-server";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    _nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    _nixpkgs-unstable,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+
+    systems = [
+      "aarch64-linux"
+      "x84_64-linux"
+    ];
+
+    hostSystems = [
+      "rpi-5"
+      "linux"
+    ];
+
+    hosts = [
+      {
+        name = "template";
+        system = "rpi-5";
+      }
+    ];
+
+    # Validate if the hostsSystem is correctly configured
+    _ =
+      map (
+        host: assert builtins.elem host.system hostSystems; host
+      )
+      hosts;
+
+    forAllSystems = fn: nixpkgs.lib.genAttrs systems (systems: fn {pkgs = import nixpkgs {inherit systems;};});
+  in {
+    formatter = forAllSystems ({pkgs}: pkgs.alejandra);
+
+    nixosConfigurations = builtins.listToAttrs (map (host: {
+        name = host.name;
+        value = nixpkgs.lib.nixosSystem {
+          specialArgs = {
+            inherit inputs outputs;
+            meta = {
+              hostname = host.name;
+              system = host.system;
+            };
+          };
+
+          system =
+            if builtins.elem "rpi" hostSystems
+            then "aarch64-linux"
+            else "x86_64-linux";
+
+          modules = [];
+        };
+      })
+      hosts);
+  };
+}
