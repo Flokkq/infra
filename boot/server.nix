30f150c582f61f9d094b50cc052febec3528cc03
Flokkq <webcla21@htl-kaindorf.at>
2025-10-09 20:02:25 +0200

    fix(boot): remove explicit grub definition from server
    Signed-off-by: Flokkq <webcla21@htl-kaindorf.at>

--- boot/server.nix
+++ boot/server.nix
@@ -2,12 +2,6 @@
 # <https://github.com/nix-community/nixos-generators?tab=readme-ov-file#format-specific-notes>
 _: {
   boot = {
-    loader.grub = {
-      enable = true;
-      efiSupport = true; # Enables UEFI support
-      devices = ["nodev"]; # Install GRUB into the image, not the current system
-    };
-
     kernelParams = ["console=tty0"]; # Log to display instead of serial console
 
     # GRUB timeout
